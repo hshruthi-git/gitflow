0efe7bbabf5500a08379dc6084bb4eaa9553dc5f
hshruthi-git <hshruthi.08@gmail.com>
2025-09-11 00:04:09 +0530

    Update jenkins_script.sh

--- jenkins_script.sh
+++ jenkins_script.sh
@@ -4,3 +4,5 @@ echo "this script is to check git automation with jenkins"
 echo " to check Poll SCM and build periodically"
 
 echo " to check webhook trigger"
+
+echo " to check webhook trigger second time"
